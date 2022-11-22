8582019835ec0babe4dc8069dbe959d3ab2a2443
Peer Duensing <p.duensing@stud.uni-hannover.de>
2022-11-22 18:36:43 +0100

    prints dictionaries at start

--- train.py
+++ train.py
@@ -136,6 +136,9 @@ else: loss_fn = utils.triplet_euclidean_loss
 param_dict = {"model": MODEL, "trained_layers": model.trained_layers, "dataset": DATASET, "epochs": EPOCHS, "batch_size": BATCH_SIZE, "learning_rate": LEARNING_RATE, "weight_decay": WEIGHT_DECAY, "optimizer": type(optimizer).__name__, "loss_fn": type(loss_fn).__name__}
 data_dict = train_dataset.state_dict
 
+print(param_dict, flush=True)
+print(data_dict, flush=True)
+
 training_dict = triplet_train(model, EPOCHS, train_dataloader, test_dataloader, loss_fn, optimizer, with_classification)
 
 if with_inference: inference_dict = inference.run_inference(model, test_dataset)
