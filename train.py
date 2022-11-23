69d43be56023cdbe55b0f08965d695b5aac244bf
Peer Duensing <p.duensing@stud.uni-hannover.de>
2022-11-23 12:36:48 +0100

    bug fix

--- train.py
+++ train.py
@@ -121,9 +121,12 @@ training_dict = {}
 param_dict = {}
 data_dict= {}
 
+img_format = 'jpg'
+if 'drawings' in args.img_type: img_format = 'png'
+
 
 # options have to be added
-train_dataset, test_dataset = data_preparation.get_datasets(dataset=DATASET, size=args.dsize, img_type=args.img_type, transform=model.transform)
+train_dataset, test_dataset = data_preparation.get_datasets(dataset=DATASET, size=args.dsize, img_type=args.img_type, img_format=img_format, transform=model.transform)
 
 train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=True) #num_workers = os.cpu_count()
 test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=False) #num_workers = os.cpu_count()
