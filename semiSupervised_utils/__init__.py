68ce27b1a43797a26daa4b1c62b9dd18a99cac35
Peer Duensing <p.duensing@stud.uni-hannover.de>
2022-12-18 23:08:04 +0100

    bugfix

--- semiSupervised_utils/__init__.py
+++ semiSupervised_utils/__init__.py
@@ -1,3 +1,3 @@
 from .svg_handler import build_svg, parse_svg, load_tuple_representation, reshape_vectorSketch
-from .training_utils import torch_2d_normal, sketch_reconstruction_loss, set_learningRate
+from .training_utils import torch_2d_normal, sketch_reconstruction_loss, set_learningRate, sketch_reconstruction_loss_withoutMask
 from .rasterize import batch_rasterize_relative
