8fc377196af2e241ad6cd34f3a4e0817f50f519f
Peer Duensing <p.duensing@stud.uni-hannover.de>
2023-01-12 15:40:11 +0100

    dilated sketch transform

--- transformations.py
+++ transformations.py
@@ -1,5 +1,10 @@
 from torchvision import transforms
 from PIL import Image
+import cv2
+import numpy as np
+
+import argparse
+from pathlib import Path
 
 image_transformV1 = transforms.Compose([
             transforms.Resize(size=(224, 224), interpolation=transforms.InterpolationMode.BICUBIC, max_size=None, antialias=None),
@@ -54,8 +59,47 @@ def get_transformation(type='image_transform', version='V1'):
     return eval(t), t
 
 
-if __name__ == '__main__':
+def test_transform(options):
     image = Image.open('../transformations/test.png')
     for i in range(10):
         augmented_img = sketch_transformV2(image)
         augmented_img.save(f'../transformations/transformed7_img_{i}.png')
+
+def dilate(options):
+    kernel = np.ones((4, 4), np.uint8)
+    print("hi")
+
+    dir = Path("data/kaggle")
+    img_dir = dir / options[0]
+    print(img_dir)
+    image_paths = list(img_dir.glob("*.png"))
+
+    new_dir = dir / f"dilated_{options[0]}"
+    if not new_dir.is_dir():
+        new_dir.mkdir(parents=True, exist_ok=True)
+
+    for path in image_paths:
+
+        img = cv2.imread(str(path))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        img = cv2.dilate(img, kernel)
+
+        img = np.asarray(img)
+        np.place(img, img>250, [255])
+        np.place(img, img<250, [0])
+
+        cv2.imwrite(str(new_dir / path.name), img)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--method', required=True, choices=['test_transform', 'dilate'])
+    parser.add_argument('-o', '--options', default=[])
+
+    args = parser.parse_args()
+    if not isinstance(args.options, list):
+        args.options = [args.options]
+
+    eval(args.method)(args.options)
